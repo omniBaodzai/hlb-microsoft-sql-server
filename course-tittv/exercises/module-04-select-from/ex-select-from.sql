d8e8504957296497212f8e26418b7b2a81cdd971
omniBaodzai <huynhlebaoes@gmail.com>
2026-02-24 23:31:17 +0700

    bai 04: cau lenh truy van select

---
+++ course-tittv/exercises/module-04-select-from/ex-select-from.sql
@@ -0,0 +1,30 @@
+﻿/* Bài tập bài 04: Câu lệnh truy vấn SELECT */
+
+/* Phần 1: Câu lệnh SELECT... FROM... */
+/* Bài tập 1: Viết câu lệnh SQL lấy ra tên và số điện thoại của tất cả các nhà cung cấp
+từ [bảng Suppliers] */
+SELECT CompanyName, Phone
+FROM Suppliers;
+
+/* Bài tập 2: Viết câu lệnh SQL lấy ra họ, tên và chức vụ của nhân viên
+từ [bảng Employees] */
+SELECT LastName, FirstName, Title
+FROM Employees;
+
+/* Bài tập 3: Viết câu lệnh SQL lấy ra danh sách danh mục sản phẩm và mô tả
+từ [bảng Categories] */
+SELECT CategoryName, Description
+FROM [dbo].[Categories];
+
+/* Phần 2: Câu lệnh SELECT * FROM... */
+-- Bài tập 1: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ [bảng Suppliers]
+SELECT *
+FROM Suppliers;
+
+-- Bài tập 2: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ [bảng Order Details]
+SELECT *
+FROM [Order Details];
+
+-- Bài tập 3: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ [bảng Shippers]
+SELECT * 
+FROM Shippers;
---
+++ course-tittv/lessons/module-04-select-from/l-select-from.sql
@@ -0,0 +1,32 @@
+﻿/* Bài 04. Câu lệnh truy vấn SELECT */
+
+/* Note:
+- Đặt ngoặc vuông với bảng có khoảng trắng: [Oder Details]
+*/
+
+/* Phần 1: Câu lệnh SELECT... FROM... */
+-- Ví dụ 1: Viết câu lệnh SQL lấy ra tên của tất cả các sản phẩm từ [bảng Products]
+SELECT ProductName
+FROM Products;
+
+SELECT [ProductName]
+FROM [dbo].[Products];
+
+/* Ví dụ 2: Viết câu lệnh SQL lấy ra tên sản phẩm, giá bán trên mỗi đơn vị,
+số lượng sản phẩm trên mỗi đơn vị từ [bảng Products] */
+SELECT ProductName, UnitPrice, QuantityPerUnit
+FROM dbo.Products;
+
+/* Ví dụ 3: Viết câu lệnh SQL lấy ra tên công ty và quốc gia của các khách hàng
+từ [bảng Customers] */
+SELECT CompanyName, Country
+FROM Customers;
+
+/* Phần 2: Câu lệnh SELECT * FROM... */
+-- Ví dụ 1: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ [bảng Products]
+SELECT *
+FROM Products;
+
+-- Ví dụ 2: Viết câu lệnh SQL lấy ra tất cả dữ liệu từ [bảng Customers]
+SELECT *
+FROM Customers;
