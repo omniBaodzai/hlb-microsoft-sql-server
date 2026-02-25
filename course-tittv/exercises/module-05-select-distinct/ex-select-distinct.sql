594b60a82d3f2f9e5c3fd354a78648b4b845f71f
omniBaodzai <huynhlebaoes@gmail.com>
2026-02-25 22:53:38 +0700

    bai 05: cau lenh truy van select distinct - lay du lieu khong trung lap

---
+++ course-tittv/exercises/module-05-select-distinct/ex-select-distinct.sql
@@ -0,0 +1,18 @@
+﻿/* Bài tập bài 05: Câu lệnh truy vấn SELECT DISTINCT lấy dữ liệu không trùng lặp */
+
+/* Bài tập 1: Viết câu lệnh SQL lấy ra các mã đơn vị vận chuyển khác nhau 
+của các đơn hàng từ [bảng Orders] */
+SELECT DISTINCT [ShipVia]
+FROM [dbo].[Orders];
+
+/* Bài tập 2: Viết câu lệnh SQL lấy ra tên các thành phố và quốc gia khác nhau
+của các nhân viên từ [bảng Employees] */
+SELECT DISTINCT [City], [Country]
+FROM [dbo].[Employees];
+
+SELECT [City], [Country]
+FROM [dbo].[Employees];
+
+-- Bài tập 3: Viết câu lệnh SQL lấy ra các ngày đặt hàng khác nhau từ [bảng Orders]
+SELECT DISTINCT [OrderDate]
+FROM [dbo].[Orders];
---
+++ course-tittv/lessons/module-05-select-distinct/l-select-distinct.sql
@@ -0,0 +1,14 @@
+﻿/* Bài 05: Câu lệnh truy vấn SELECT DISTINCT lấy dữ liệu không trùng lặp */
+
+-- Ví dụ 1: Viết câu lệnh SQL lấy ra tên các quốc gia khác nhau từ [bảng Customers]
+SELECT DISTINCT [Country]
+FROM [dbo].[Customers];
+
+-- Ví dụ 2: Viết câu lệnh SQL lấy ra tên các mã bưu điện khác nhau từ [bảng Suppliers]
+SELECT DISTINCT [PostalCode]
+FROM [dbo].[Suppliers];
+
+/* Ví dụ 3: Viết câu lệnh SQL lấy ra các dữ liệu khác nhau về họ và danh xưng lịch sự
+của các nhân viên từ [bảng Employees] */
+SELECT DISTINCT [LastName], [TitleOfCourtesy]
+FROM [dbo].[Employees];
