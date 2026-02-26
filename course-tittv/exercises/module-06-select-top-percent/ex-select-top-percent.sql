2294dc81c6453ae258b6b7e437e60fdfad65ccef
omniBaodzai <huynhlebaoes@gmail.com>
2026-02-27 00:07:03 +0700

    bai 06: cau lenh truy van select top percent

---
+++ course-tittv/exercises/module-06-select-top-percent/ex-select-top-percent.sql
@@ -0,0 +1,18 @@
+﻿/* Bài tập bài 06: Câu lệnh truy vấn SELECT TOP giới hạn dòng trả về */
+
+/* Bài tập 1: Viết câu lệnh SQL lấy ra các sản phẩm có mã danh mục không bị
+trùng lặp và chỉ lấy ra 3 dòng đầu tiên từ [bảng Products] */
+SELECT DISTINCT TOP 3 [CategoryID]
+FROM [dbo].[Products];
+
+/* Bài tập 2: Viết câu lệnh SQL lấy ra 50% danh sách các công ty từ [Bảng Suppliers] */
+SELECT TOP 30 PERCENT [CompanyName]
+FROM [dbo].[Suppliers];
+
+/* Bài tập 3: Viết câu lệnh SQL lấy ra danh sách thành phố có đơn hàng sẽ được giao
+đến từ [bảng Orders] với quy định:
+- Các thành phố khác nhau, không được trùng lặp
+- Chỉ lấy 20 dòng đầu tiên
+*/
+SELECT DISTINCT TOP 20 [ShipCity]
+FROM [dbo].[Orders];
---
+++ course-tittv/lessons/module-06-select-top-percent/l-select-top-percent.sql
@@ -0,0 +1,16 @@
+﻿/* Bài 06: Câu lệnh truy vấn SELECT TOP giới hạn dòng trả về */
+
+-- Ví dụ 1: Viết câu lệnh SQL lấy ra 5 dòng đầu tiên từ [bảng Customers]
+SELECT TOP 5 *
+FROM [dbo].[Customers];
+
+-- Ví dụ 2: Viết câu lệnh SQL lấy ra 30% nhân viên từ [bảng Employees]
+SELECT TOP 30 PERCENT *
+FROM [dbo].[Employees];
+
+/* Ví dụ 3: Viết câu lệnh SQL lấy ra mã khách hàng từ [bảng Orders] với quy định:
+- Mã khách hàng không được trùng lặp
+- Chỉ lấy ra 5 dòng đầu tiên
+*/
+SELECT DISTINCT TOP 5 [CustomerID]
+FROM [dbo].[Orders];
